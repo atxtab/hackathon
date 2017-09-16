00acd54d8e57a9c18973a1b8cb3f6e0fd14871ee
Brigitte Huneke <brigittehuneke@gmail.com>
2017-09-16 12:29:51 -0500

    added homepage

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 //import logo from './logo.svg';
 import '../App.css';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
-import UserProfile from './UserProfile';
+import Home from './Home';
 
 class App extends Component {
   render() {
@@ -12,7 +12,7 @@ class App extends Component {
           <div className="App-header">  
             <h2>Food Truck App</h2>
           </div>
-          <Route exact path="/" component={UserProfile}/>
+          <Route exact path="/" component={Home}/>
         </div>
       </Router>
     );
---
+++ client/src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Route } from 'react-router-dom';
+
+function Home(props) {
+  return (
+    <main>
+      <button>Vendor</button>
+      <button>Customer</button>
+    </main>
+  )
+}
+
+export default Home;
