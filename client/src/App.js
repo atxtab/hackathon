fe71d38cd3c19e6b6605408608f0012d73f14bd3
Brigitte Huneke <brigittehuneke@gmail.com>
2017-09-16 10:35:40 -0500

    created react app with eslint

---
+++ client/src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+//import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          
+          <h2>Food Truck App</h2>
+        </div>
+        <p className="App-intro">
+          Main Page
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
+
+//<img src={logo} className="App-logo" alt="logo" />ß
